931118e1d71e65dd769611162cff23b55442db4a
Jeff <jeff@jumbledbytes.com>
2018-12-30 10:17:13 -0800

    Get data server url from config file

--- client/src/datasources/apollo-datasource.ts
+++ client/src/datasources/apollo-datasource.ts
@@ -1,17 +1,36 @@
 import ApolloClient from "apollo-boost";
+import config from "./apollo-datasource.json";
 
 class ApolloDatasource {
   private defaultServerUrl = `${window.location.protocol}//${window.location.hostname}:4001/`;
   private serverUrl: string;
   private _client: ApolloClient<any>;
+  private apolloConfig = config;
 
   constructor(url?: string) {
-    this.serverUrl = url || this.defaultServerUrl;
+    this.serverUrl = url || this.getServerUrl();
     this._client = new ApolloClient({ uri: this.serverUrl });
   }
 
   public get client(): ApolloClient<any> {
     return this._client;
+  }
+
+  private getServerUrl() {
+    let hostname = this.apolloConfig.serverUrl;
+    if (hostname === "${window.location.hostname}") {
+      hostname = window.location.hostname;
+    }
+    let hostNameMutator = this.apolloConfig.serverUrlRegex;
+    if (hostNameMutator && hostNameMutator.length >= 2) {
+      hostname = hostname.replace(hostNameMutator[0], hostNameMutator[1]);
+    }
+    if (!hostname) {
+      hostname = this.defaultServerUrl;
+    }
+    const portString = this.apolloConfig.serverPort > 0 ? `:${this.apolloConfig.serverPort}` : "";
+    const serverUrl = `${window.location.protocol}//${hostname}${portString}`;
+    return serverUrl;
   }
 }
 
