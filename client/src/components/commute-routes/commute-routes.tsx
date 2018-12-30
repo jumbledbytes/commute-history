3100c248bc00c005256ace877e7e8afe6844107e
Jeff <jeff@jumbledbytes.com>
2018-12-29 17:04:47 -0800

    Change route on tab click

--- client/src/components/commute-routes/commute-routes.tsx
+++ client/src/components/commute-routes/commute-routes.tsx
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, SyntheticEvent } from "react";
+
+import { withRouter, RouterChildContext, RouteComponentProps } from "react-router-dom";
 
 import { Tab } from "semantic-ui-react";
 
 import IRoute from "../../../../common/models/iroute";
 import TravelTimesController from "../../controllers/travel-times-controller";
 
-interface ICommuteRoutesProps {
+interface ICommuteRoutesProps extends RouteComponentProps {
   routes: Array<IRoute>;
   currentRoute?: string;
 }
 
 class CommuteRoutes extends Component<ICommuteRoutesProps> {
   public static defaultProps = {
-    routes: []
+    routes: [],
+    currentRoute: undefined
+  };
+
+  private onTabChange = (event: SyntheticEvent, data: any) => {
+    const { routes } = this.props;
+    this.props.history.push(`/${routes[data.activeIndex].routeName}`);
   };
 
   public render() {
@@ -24,8 +32,16 @@ class CommuteRoutes extends Component<ICommuteRoutesProps> {
     const tabPanes = routes.map(route => {
       return { menuItem: route.routeName, render: () => <TravelTimesController routeName={route.routeName} /> };
     });
-    return <Tab style={{ height: "90%" }} panes={tabPanes} renderActiveOnly={true} activeIndex={routeIndex} />;
+    return (
+      <Tab
+        style={{ height: "90%" }}
+        panes={tabPanes}
+        renderActiveOnly={true}
+        activeIndex={routeIndex}
+        onTabChange={this.onTabChange}
+      />
+    );
   }
 }
 
-export default CommuteRoutes;
+export default withRouter(CommuteRoutes);
