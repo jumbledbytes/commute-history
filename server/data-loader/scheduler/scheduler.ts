d5338064d21c9b2d1eb1e75abc8f00c32086b2fc
Jeff <jeff@jumbledbytes.com>
2019-02-08 12:07:54 -0800

    Minor cleanup to make schedule more resilient

--- server/data-loader/scheduler/scheduler.ts
+++ server/data-loader/scheduler/scheduler.ts
@@ -6,7 +6,11 @@ const config = JSON.parse(scheduleJson.toString());
 
 const loadData = () => {
   console.log(new Date().toString() + ": loading data ");
-  execSync("yarn load-data");
+  try {
+    execSync("yarn load-data");
+  } catch (e) {
+    console.error(`Unable to load data: ${e.message}`);
+  }
 };
 
 loadData();
